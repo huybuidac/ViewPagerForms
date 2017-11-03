6cc24878fe1037b6e0abfaf1bc9a5a66b8736ff1
Huy Bui Dac <huybd1@fsoft.com.vn>
2017-11-03 23:46:47 +0700

    Add Demo project

---
+++ ViewPagerDemo/ViewPagerDemoPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ViewPagerDemo
+{
+    public partial class ViewPagerDemoPage : ContentPage
+    {
+        public ViewPagerDemoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
